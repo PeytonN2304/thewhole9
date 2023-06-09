ddc0699c27db6e42180da18883f547c6c7920890
PeytonN2304 <peytonknewton@gmail.com>
2023-06-09 01:36:38 -0400

    yes

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 function toggleMobileMenu(menu) {
   menu.classList.toggle("open");
 }
+
+const addToCartButton = document.querySelectorAll("[data-addToCartButton]");
+const cartQuantity = document.querySelector("[data-cartQuantity]")
+
+
+
+addToCartButton.addEventListener('click', function() {
+  // Get the current quantity value
+  var currentQuantity = parseInt(quantityElement.getAttribute('value'));
+
+  // Increment the quantity by 1
+  var newQuantity = currentQuantity + 1;
+
+  // Update the quantity element
+  cartQuantity.innerHTML = newQuantity;
+  cartQuantity.setAttribute('value', newQuantity.toString());
+});
+
+
+
